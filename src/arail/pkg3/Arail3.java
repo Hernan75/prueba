412c655475ce43872640c24543da5838e7133d9b
Hernan <hernanelizondo75@hotmail.com>
2021-10-15 09:41:30 -0300

    Primer commit

---
+++ src/arail/pkg3/Arail3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arail.pkg3;
+
+/**
+ *
+ * @author herna
+ */
+public class Arail3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[]edad={40,20,4};
+        for (int i = 0; i < edad.length; i++) {
+            System.out.println("la edad es: "+edad[i]);            
+        }
+    }
+    
+}
